a17c504ab81dd6d861d2004af09b295c259f1e69
Emanuel0-tech <manelsilva0830@gmail.com>
2024-09-05 00:05:22 -0300

    AJ LANCHES, AGORA VAI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'home': "url('/assets/bg.png')"
-      }
     },
   },
   plugins: [],
